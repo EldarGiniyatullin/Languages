f162f06592fcbb642ce245608f7a536166cab840
Eldar <ric1995@mail.ru>
2016-03-28 22:59:48 +0400

    List comprehension and ?:

---
+++ Python/for and if-else.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[4]:
+
+a, b = (int(i) for i in input().split())
+print(True if a == b else False)
+
+
+# In[5]:
+
+genome = 'ATGG'
+# genome[2] = 'S' -- mistake
+for i in genome:
+    print(i)
+
+
+# In[ ]:
+
+
+
